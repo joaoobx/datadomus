89e72acfc1483ea9249b7c46098fa3ce97582988
João Marcos de Oliveira Barbosa <joaoobx@gmail.com>
2024-04-02 09:46:30 -0300

    useful phones

--- boards/models.py
+++ boards/models.py
@@ -36,3 +36,15 @@ class NoticeBoard(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, max_length=100, null=False, blank=False)
     updated_at = models.DateTimeField(auto_now=True, max_length=100)
     deleted_at = models.DateTimeField(null=True)
+
+class UsefulPhonesBoard(models.Model):
+    id = models.UUIDField( 
+         primary_key = True, 
+         default = uuid.uuid4, 
+         editable = False)
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    doc_url = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True, max_length=100, null=False, blank=False)
+    updated_at = models.DateTimeField(auto_now=True, max_length=100)
+    deleted_at = models.DateTimeField(null=True)
